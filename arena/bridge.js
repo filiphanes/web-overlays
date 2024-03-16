3f590c530531328b6696d5ec9e74d67228769002
Filip Hanes <filip.hanes@protonmail.com>
2024-03-16 10:33:43 +0100

    find_clips each 10s

--- arena/bridge.js
+++ arena/bridge.js
@@ -14,7 +14,7 @@ async function put_clip(id, data) {
             console.error("Arena: PUT failed", await response.text());
         }
     } catch (error) {
-        console.error("Arena: Connection error", error);
+        console.error("put clip error", error.code);
     }
 }
 
@@ -26,23 +26,31 @@ async function set_video_opacity(id, value) {
     await put_clip(id, { "video": { "opacity": value } })
 }
 
-// Init Arena clips
+async function find_clips() {
+    try {
+        var response = await fetch(`${config.arena_url}/api/v1/composition`)
+    } catch (error) {
+        console.log("find clips error", error.code);
+        return;
+    }
+    const data = await response.json();
+    data.layers.forEach((layer) => {
+        layer.clips.forEach((clip) => {
+            if      (clip.name.value.includes("#line1")) clip_ids.line1 = clip.id;
+            else if (clip.name.value.includes("#line2")) clip_ids.line2 = clip.id;
+        })
+    })
+    console.log("clip_ids", clip_ids)
+}
+
 let clip_ids = {
     line1: 0,
     line2: 0,
 }
-const response = await fetch(`${config.arena_url}/api/v1/composition`)
-const data = await response.json();
-for (let i=0; i<data.layers.length; i++) {
-    const layer = data.layers[i].clips;
-    for (let j=0; j<layer.length; j++) {
-        clip_ids[layer[j].name.value] = layer[j].id;
-    }
-}
-console.log("clips", clip_ids)
-
 let gun = Gun(config.gun_peers);
 let overlay = gun.get('bible').get(config.namespace || demo);
+await find_clips();
+setInterval(find_clips, 10101);
 
 overlay.get('line1').on(function(data, key){
     set_clip_text(clip_ids.line1, data.trim());
